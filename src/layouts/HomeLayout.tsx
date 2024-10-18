d0519060306a232ea495d38e8f359daa42afcde0
miguel.mamani <miguel.mamani.syc@endetransmision.bo>
2024-10-18 18:34:30 -0400

    demo

---
+++ src/layouts/HomeLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SidebarProvider } from '@/components/ui/sidebar'
+
+export const HomeLayout: React.FC = () => {
+  return (
+    <SidebarProvider>
+        </SidebarProvider>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import queryClient from "./queryClient.ts";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "./index.css";
+import Demo from "./Demo.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Demo />
       <ReactQueryDevtools />
     </QueryClientProvider>
   </StrictMode>
